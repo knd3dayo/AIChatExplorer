563b3672d8434eaa3861bce12a4e8918ee7f126a
user <user@DESKTOP-QK8T16E>
2024-05-05 12:15:33 +0900

    レイアウト修正

--- WpfAppCommon/python/clipboard_app_faiss.py
+++ WpfAppCommon/python/clipboard_app_faiss.py
@@ -4,16 +4,18 @@ import os
 index_file_name="index.faiss"
 index = None
 
+# faissのindexをロードする関数
 def load_faiss_index():
     global index
     # indexファイルが存在しない場合はindexを作成する
-    if os.path.exists(index_file_name) == False:
+    if not os.path.exists(index_file_name):
         index = faiss.IndexFlatL2(1536)
     else:
         index = faiss.read_index(index_file_name)
-        
+            
     return index
 
+# faissのindexを保存する関数
 def save_faiss_index():
     faiss.write_index(index, index_file_name)
     
