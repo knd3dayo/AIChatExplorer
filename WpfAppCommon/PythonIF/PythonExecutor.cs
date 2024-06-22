c55e1936574964feace25ee96a280f2c544db0f3
user <user@DESKTOP-QK8T16E>
2024-06-23 06:01:24 +0900

    update

--- WpfAppCommon/PythonIF/PythonExecutor.cs
+++ WpfAppCommon/PythonIF/PythonExecutor.cs
@@ -7,19 +7,6 @@ namespace WpfAppCommon.PythonIF {
         // String definition instance
         public static StringResources StringResources { get; } = StringResources.Instance;
 
-        public enum PythonExecutionType {
-            None = 0,
-            PythonNet = 1,
-            InternalFlask = 2,
-            ExternalFlask = 3
-        }
-        public static PythonExecutionType PythonExecution {
-            get {
-                // Convert from int to PythonExecutionType
-                return (PythonExecutionType)ClipboardAppConfig.PythonExecute;
-            }
-
-        }
 
         // Template file for custom Python scripts
         public static string TemplateScript { get; } = StringResources.TemplateScript;
@@ -30,11 +17,7 @@ namespace WpfAppCommon.PythonIF {
         public static IPythonFunctions PythonFunctions { get; set; } = new EmptyPythonFunctions();
         public static void Init(string pythonPath) {
 
-
-            // If PythonExecution in WpfAppCommonSettings is PythonNet, execute InitPythonNet
-            if (PythonExecution == PythonExecutionType.PythonNet) {
-                PythonFunctions = new PythonNetFunctions(pythonPath);
-            }
+            PythonFunctions = new PythonNetFunctions(pythonPath);
         }
 
         // Load Python script
