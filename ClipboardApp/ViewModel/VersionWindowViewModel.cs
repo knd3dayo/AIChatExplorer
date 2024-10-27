10b39f9d34bf54f6ad9b8914a31892240cd378f9
user <user@DESKTOP-QK8T16E>
2024-10-28 03:21:08 +0900

    文字列リソースの修正

--- ClipboardApp/ViewModel/VersionWindowViewModel.cs
+++ ClipboardApp/ViewModel/VersionWindowViewModel.cs
@@ -11,7 +11,7 @@ namespace ClipboardApp.ViewModel {
 
         public string Title { get; set; } = CommonStringResources.Instance.VersionInformation;
         public VersionWindowViewModel() {
-            Version = "0.2.1";
+            Version = "0.3.0";
             CopyRight = "© 2024 by knd3dayo";
             Url = "https://github.com/knd3dayo/ClipboardApp";
         }
