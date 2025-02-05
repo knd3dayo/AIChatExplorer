1306f7b47d57330404ae54bf3eb74ce3105229cd
user <user@DESKTOP-QK8T16E>
2025-02-06 00:23:09 +0900

    レイアウト修正

--- LibUIPythonAI/View/ChatMain/ChatControl.xaml.cs
+++ LibUIPythonAI/View/ChatMain/ChatControl.xaml.cs
@@ -4,8 +4,8 @@ namespace LibUIPythonAI.View.ChatMain {
     /// <summary>
     /// QAChatControl.xaml の相互作用ロジック
     /// </summary>
-    public partial class QAChatControl : UserControl {
-        public QAChatControl() {
+    public partial class ChatControl : UserControl {
+        public ChatControl() {
             InitializeComponent();
         }
     }
