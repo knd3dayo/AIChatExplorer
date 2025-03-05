329f74ddd56d271d2c3f12662d2a3a6ff1727938
user <user@DESKTOP-QK8T16E>
2025-03-06 05:10:17 +0900

    update

--- LibPythonAI/Data/PythonAILibDBContext.cs
+++ LibPythonAI/Data/PythonAILibDBContext.cs
@@ -4,7 +4,7 @@ using PythonAILib.Common;
 namespace LibPythonAI.Data {
     public class PythonAILibDBContext : DbContext {
 
-        public DbSet<ContentFolderEntity> Folders { get; set; }
+        public DbSet<ContentFolderEntity> ContentFolders { get; set; }
 
         public DbSet<ContentItemEntity> ContentItems { get; set; }
 
@@ -31,44 +31,9 @@ namespace LibPythonAI.Data {
         }
 
         public static void Init() {
-            using (var context = new PythonAILibDBContext()) {
-                context.Database.EnsureDeleted();
-                context.Database.EnsureCreated();
-
-                // Create ContentFolder
-                ContentFolderEntity rootFolder = new() {
-                    FolderName = "Root",
-                    IsRootFolder = true
-                };
-                // Children
-                ContentFolderEntity childFolder = new() {
-                    FolderName = "Child",
-                    ParentId = rootFolder.Id
-                };
-                context.Folders.Add(rootFolder);
-                context.Folders.Add(childFolder);
-
-                context.SaveChanges();
-
-                // Querying child folder using LINQ
-                ContentFolderEntity? child = context.Folders.Where(f => f.FolderName == "Child").FirstOrDefault();
-                if (child != null) {
-                    Console.WriteLine($"Child folder found: {child.FolderName}");
-                    // Get root folder by child.Parent using LINQ
-                    ContentFolderEntity? root = child.Parent;
-                    if (root != null) {
-                        Console.WriteLine($"Root folder found: {root.FolderName}");
-                    } else {
-                        Console.WriteLine("Root folder not found");
-                    }
-
-
-                } else {
-                    Console.WriteLine("Child folder not found");
-                }
-
-
-            }
+            using var context = new PythonAILibDBContext();
+            context.Database.EnsureCreated();
+            context.SaveChanges();
         }
     }
 }
